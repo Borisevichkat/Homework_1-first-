b178ea001d390b2264e66014cee191576c05b3ef
Borisevichkat <52928737+Borisevichkat@users.noreply.github.com>
2021-02-01 16:56:50 +0300

    Add files via upload

---
+++ task9/Task_9.java
@@ -0,0 +1,15 @@
+package epam.training.homework1.task9;
+import java.io.IOException;
+
+public class Task_9 {
+
+    public static void main(String[] args) throws IOException {
+        double radius = Solution_9.readInt("Введите радиус:");
+        double circumference = Solution_9.calculateCircumference(radius);
+        double area = Solution_9.calculateArea(radius);
+        Solution_9.printResult(circumference, area);
+    }
+
+}
+
+
